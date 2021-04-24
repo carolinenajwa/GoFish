05aa3de2dbdb28f5fdf71c9dd22352b8b0b8fef3
Odyssey-94 <82617020+Odyssey-94@users.noreply.github.com>
2021-04-23 21:45:13 -0700

    Create GoFishMain.java

---
+++ GoFishMain.java
@@ -0,0 +1,29 @@
+iport java.util.ArrayList; 
+import java.util.Scanner;  
+import java.util.Random;
+
+
+public class GoFishMain
+{
+    //static final Random __ = new Random();
+    static private ArrayList<Card> cards;
+    static public Player[] Players;
+ 
+    // Returns cards - removes them from deck
+    public static Card draw()
+
+ 
+  // Returns Deck Size
+	public static int deckSize()
+
+ 
+    // Main
+    public static void main(String[] args)
+  
+  
+ 
+    // Play Game - keep playing if 13 < suits found
+  
+  
+    // Print Winner
+
